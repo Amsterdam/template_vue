6eae3d9fe424e79b315750a0eae77a9342b7d234
vegter <amsjurien@gmail.com>
2018-04-09 12:24:40 +0200

    Removed mapCoverage option from jest config Fixed HelloWorld unit test

--- template/test/unit/specs/HelloWorld.spec.js
+++ template/test/unit/specs/HelloWorld.spec.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
+  it('should mount', () => {
     const Constructor = Vue.extend(HelloWorld)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      {{#if_eq runner "karma"}}.to.equal('Welcome to Your Vue.js App'){{/if_eq}}{{#if_eq runner "jest"}}.toEqual('Welcome to Your Vue.js App'){{/if_eq}}
+    expect(vm.$options.name).toEqual('HelloWorld')
   })
 })
